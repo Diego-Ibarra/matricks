f6751b5850f5c120572593e33567eaa3018a0716
Diego Ibarra <Diego.Ibarra@dal.ca>
2018-03-15 11:06:56 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""
+Good place to read about packaging:
+https://python-packaging-user-guide.readthedocs.org/en/latest/distributing.html
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='ODIN',
+      version='1.0',
+      packages=find_packages(),
+      )
